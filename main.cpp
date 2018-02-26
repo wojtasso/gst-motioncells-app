beec531d79e4c6a45712bc792dd67af2f50728bf
Wojciech Tatarski <w.tatarski93@wp.pl>
2018-02-26 21:59:30 +0100

    Added motion detection for frames from USB camera

--- main.cpp
+++ main.cpp
@@ -6,12 +6,22 @@
 
 #define UDPSRC_CAPS "application/x-rtp, encoding-name=JPEG, payload=26"
 
-
 using namespace std;
 
 typedef struct __CustomData {
-    GstElement *video_queue, *motion_queue, *pipeline,*sink1, *source, *rtpjpeg, *decoder,
-               *videoparse1, *videoparse2, *motion, *tee, *sink, *opencvtextoverlay;
+    GstElement *video_queue,
+               *motion_queue,
+               *pipeline,
+               *sink1,
+               *source,
+               *rtpjpeg,
+               *decoder,
+               *videoparse1,
+               *videoparse2,
+               *motion,
+               *tee,
+               *sink,
+               *opencvtextoverlay;
     GstBus *bus;
     guint bus_watch_id;
 
@@ -108,7 +118,6 @@ static gboolean bus_call (GstBus *bus, GstMessage *msg, CustomData *data){
 
 int main(int argc, char *argv[])
 {
-    cout << "Hello !!!  "<< endl;
     CustomData data;
     GIOChannel *io_stdin;
 
@@ -117,11 +126,11 @@ int main(int argc, char *argv[])
     data.loop = g_main_loop_new (NULL, FALSE);
 
     data.pipeline = gst_pipeline_new ("streaming");
-    data.source = gst_element_factory_make ("udpsrc","videosource");
+    data.source = gst_element_factory_make ("v4l2src","videosource");
     data.rtpjpeg = gst_element_factory_make ("rtpjpegdepay", "rtp");
     data.decoder = gst_element_factory_make ("jpegdec", "jpegdecoder");
-    data.videoparse1 = gst_element_factory_make("videoparse","videoparse1");
-    data.videoparse2 = gst_element_factory_make("videoparse","videoparse2");
+    data.videoparse1 = gst_element_factory_make("autovideoconvert","videoparse1");
+    data.videoparse2 = gst_element_factory_make("autovideoconvert","videoparse2");
     data.motion = gst_element_factory_make("motioncells", "motion");
     data.sink = gst_element_factory_make ("autovideosink", "sink");
     data.sink1 = gst_element_factory_make ("autovideosink", "sink1");
@@ -136,18 +145,18 @@ int main(int argc, char *argv[])
 
     g_object_set(data.tee, "name", "local", NULL);
 
-    g_object_set(data.source, "port", 5000, NULL);
+    //g_object_set(data.source, "port", 5000, NULL);
     g_object_set(data.sink, "sync", false, NULL);
     g_object_set(data.sink1, "sync", false, NULL);
 
-    g_object_set(data.videoparse1, "format", 15, NULL );	//GST_VIDEO_FORMAT_RGB
-    g_object_set(data.videoparse1, "width", 640, NULL );
-    g_object_set(data.videoparse1, "height", 480, NULL );
+    //g_object_set(data.videoparse1, "format", 15, NULL );	//GST_VIDEO_FORMAT_RGB
+    //g_object_set(data.videoparse1, "width", 640, NULL );
+    //g_object_set(data.videoparse1, "height", 480, NULL );
     //g_object_set(videoparse1, "framesize", 900000, NULL );
 
-    g_object_set(data.videoparse2, "format",  2, NULL ); //GST_VIDEO_FORMAT_I420
-    g_object_set(data.videoparse2, "width", 640, NULL );
-    g_object_set(data.videoparse2, "height", 480, NULL );
+    //g_object_set(data.videoparse2, "format",  2, NULL ); //GST_VIDEO_FORMAT_I420
+    //g_object_set(data.videoparse2, "width", 640, NULL );
+    //g_object_set(data.videoparse2, "height", 480, NULL );
 
 
     g_object_set(data.motion, "gridx", 8, NULL );
@@ -160,17 +169,17 @@ int main(int argc, char *argv[])
     g_object_set(data.motion, "postallmotion", false, NULL);
     //g_object_set(data.motion, "motionmaskcoords", "1:1,5:5", NULL);
 
-    gchar *udpsrc_caps_text;
-    GstCaps *udpsrc_caps;
-    udpsrc_caps_text = g_strdup_printf(UDPSRC_CAPS);
-    udpsrc_caps = gst_caps_from_string(udpsrc_caps_text);
-    g_object_set(data.source, "caps", udpsrc_caps, NULL);
-    gst_caps_unref(udpsrc_caps);
-    g_free(udpsrc_caps_text);
+    //gchar *udpsrc_caps_text;
+    //GstCaps *udpsrc_caps;
+    //udpsrc_caps_text = g_strdup_printf(UDPSRC_CAPS);
+    //udpsrc_caps = gst_caps_from_string(udpsrc_caps_text);
+    //g_object_set(data.source, "caps", udpsrc_caps, NULL);
+    //gst_caps_unref(udpsrc_caps);
+    //g_free(udpsrc_caps_text);
 
 
     if(!data.pipeline || !data.source || !data.rtpjpeg || !data.decoder || !data.videoparse1 ||
-            !data.tee || !data.videoparse2 ||	!data.motion || !data.sink ){
+            !data.tee || !data.videoparse2 || !data.motion || !data.sink ){
         g_printerr ("Blad");
         return -1;
     }
@@ -178,11 +187,7 @@ int main(int argc, char *argv[])
     data.bus = gst_pipeline_get_bus (GST_PIPELINE (data.pipeline));
     //bus_watch_id = gst_bus_add_watch (bus, bus_call, loop);
     /* Add a keyboard watch so we get notified of keystrokes */
-#ifdef _WIN32
-    io_stdin = g_io_channel_win32_new_fd (fileno (stdin));
-#else
     io_stdin = g_io_channel_unix_new (fileno (stdin));
-#endif
 
     data.bus_watch_id=g_io_add_watch (io_stdin, G_IO_IN, (GIOFunc)handle_keyboard, data.loop);
     GDestroyNotify notify;
@@ -194,14 +199,23 @@ int main(int argc, char *argv[])
       data.motion, data.videoparse2, data.sink, NULL);
       gst_element_link_many (data.source, data.rtpjpeg, data.decoder, data.videoparse1, data.motion,
       data.videoparse2, data.sink, NULL);*/
-    gst_bin_add_many(GST_BIN(data.pipeline), data.source, data.rtpjpeg, data.decoder, data.motion_queue,
-            data.video_queue, data.tee, data.videoparse1, data.motion, data.videoparse2, data.sink,
-            data.sink1, NULL);
-    gst_element_link_many (data.source, data.rtpjpeg, data.decoder,  data.videoparse1, data.motion, data.tee, NULL);
-    gst_element_link_many (data.video_queue, data.sink, NULL);
-    gst_element_link_many(  data.motion_queue, data.videoparse2, data.sink1, NULL);
+    gst_bin_add_many(GST_BIN(data.pipeline),
+            data.source,
+            data.videoparse1,
+            data.motion,
+            data.videoparse2,
+            data.sink,
+            NULL);
+    gst_element_link_many(data.source,
+            data.videoparse1,
+            data.motion,
+            data.videoparse2,
+            data.sink,
+            NULL);
+    //gst_element_link_many (data.video_queue, data.sink, NULL);
+    //gst_element_link_many(  data.motion_queue, data.videoparse2, data.sink1, NULL);
 
-    GstPadTemplate *tee_src_pad_template;
+    /*GstPadTemplate *tee_src_pad_template;
     GstPad *tee_q1_pad, *tee_q2_pad;
     GstPad *q1_pad, *q2_pad;
 
@@ -237,7 +251,7 @@ int main(int argc, char *argv[])
     }
 
     gst_object_unref (q1_pad);
-    gst_object_unref (q2_pad);
+    gst_object_unref (q2_pad);*/
 
     g_print ("Now playing\n");
     gst_element_set_state (data.pipeline, GST_STATE_PLAYING);
@@ -247,10 +261,10 @@ int main(int argc, char *argv[])
     g_main_loop_run (data.loop);
 
     /*Out of the main loop, clean up nicely*/
-    gst_element_release_request_pad (data.tee, tee_q1_pad);
-    gst_element_release_request_pad (data.tee, tee_q2_pad);
-    gst_object_unref (tee_q1_pad);
-    gst_object_unref (tee_q2_pad);
+    //gst_element_release_request_pad (data.tee, tee_q1_pad);
+    //gst_element_release_request_pad (data.tee, tee_q2_pad);
+    //gst_object_unref (tee_q1_pad);
+    //gst_object_unref (tee_q2_pad);
 
     g_print ("Returned, stopping playback\n");
     gst_element_set_state (data.pipeline, GST_STATE_NULL);
@@ -262,8 +276,5 @@ int main(int argc, char *argv[])
 
     g_main_loop_unref (data.loop);
 
-
-
     return 0;
-
 }
